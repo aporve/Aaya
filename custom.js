28500f3d732a1cfdc6f7026e9a1466b2e18048b2
aporve <aporvep@gmail.com>
2023-05-17 18:07:40 +0530

    Initial commit

---
+++ custom.js
@@ -0,0 +1,24 @@
+
+(function injectJS() {
+  try {
+    injectDynamicCssToChild();
+  } catch (e) {
+    console.error("failed while inserting to iFrame", e);
+  }
+})();
+
+
+function injectDynamicCssToChild() {
+  var ymFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];
+  var modularStyles = document.createElement('style');
+  modularStyles.type = 'text/css';
+  var css = '@font-face {font-family: "Formus"; src: url("/fonts/Faruma(2).otf") format("otf");} *{font-family: "Formus"; color:red;}';
+  if (modularStyles.styleSheet) {
+    modularStyles.styleSheet.cssText = css;
+  } else {
+    modularStyles.appendChild(document.createTextNode(css));
+  }
+  ymFrameHead.appendChild(modularStyles);
+}
+
+
